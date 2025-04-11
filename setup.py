6ce54d3edfa2166ed704190aa10a305b64c539f1
llorensvilla <llovigo@gmail.com>
2025-04-11 13:58:40 +0200

    New name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='geopredictor',
+    name='geopocket',
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     entry_points={
         'console_scripts': [
-            'geopredictor = geopredictor.predictor:main',
+            'geopocket = geopocket.predictor:main',
             #                      ^ paquete     ^ archivo .py sin extensión
             #                                    ^ función main() dentro de predictor.py
         ],
